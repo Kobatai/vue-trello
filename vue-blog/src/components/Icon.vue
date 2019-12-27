5bc5cc7784624c5dc0a5f3fa43d182fe36ab06f7
Kobatai <taiga1983346@gmail.com>
2019-12-27 11:49:12 +0900

    iconのコンポーネント化

---
+++ vue-blog/src/components/Icon.vue
@@ -0,0 +1,19 @@
+<template>
+  <span class="icon">
+    <i :class="['fas', nameClass]"></i>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "pm-icon",
+  props: { name: Srting },
+  // tpl内で読み取れる関数
+  // ここではFontawesomeのfaを返す
+  computed: {
+    nameClass() {
+      return `fa-${this.name}`;
+    }
+  }
+};
+</script>
