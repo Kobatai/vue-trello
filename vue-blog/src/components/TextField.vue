9267471b2472ecb0da7ecad314f02bb1fc8d95a0
Kobatai <taiga1983346@gmail.com>
2019-12-28 20:40:16 +0900

    textFieldコンポーネント化

---
+++ vue-blog/src/components/TextField.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="field">
+    <!-- ラベルの文字列が指定された時だけ表示 -->
+    <label class="label" v-if="label">{{ label }}</label>
+    <!-- iconが指定された時だけ必要なクラスを設定 -->
+    <p class="control" :class="{ 'has-icons-left' : icon }">
+      <input class="input" :type="type" :placeholder="placeholder" :value="value" @input="input" />
+      <!-- アイコンが指定されたときだけ表示 -->
+      <pm-icon v-if="icon" class="is-small is-left" :name="icon"></pm-icon>
+    </p>
+  </div>
+</template>
+
+<script>
+import pmIcon from "./Icon";
+
+export default {
+  name: "pm-text-field",
+  components: { pmIcon },
+  props: {
+    // type: String;で省略できる
+    // propsのtypeオプションは受け取るデータの型の指定
+    type: {
+      type: String,
+      label: String,
+      //親コンポーネントから指定されなかった場合にdefaultで入るもの
+      // ここではinputタグなので<input type="text">のように展開される
+      default: "text",
+      // typeの属性値がvalidatorで指定したもの以外を受け取るとconsoleにエラーを出す
+      validator(val) {
+        return ["text", "email", "password", "search", "url"].includes(val);
+      }
+    },
+    placeholder: String,
+    value: String,
+    icon: String
+  },
+  methods: {
+    input(e) {
+      if (e.target.value !== this.value) {
+        this.$emit("input", e.target.value);
+      }
+    }
+  }
+};
+</script>
