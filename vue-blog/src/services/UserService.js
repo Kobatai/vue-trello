3030c5a9398e2b424544b646a432387a968610dd
Kobatai <taiga1983346@gmail.com>
2019-12-29 21:11:09 +0900

    currentUser取得メソッド追加

--- vue-blog/src/services/UserService.js
+++ vue-blog/src/services/UserService.js
@@ -6,6 +6,26 @@ class UserService {
     this.db = firebase.firestore();
   }
 
+  async getCurrentUser() {
+    if (this.auth.currentUser == null) {
+      throw new Error("サインインしていません。　サインインしてください");
+    }
+
+    // firestoreからuserを取得
+    const snapshot = await this.db
+      .collection("users")
+      .where("authId", "==", this.auth.currentUser.uid)
+      .get();
+
+    // Firestoreのデータからユーザーオブジェクトを作成
+    let user = null;
+    snapshot.forEach(doc => {
+      user = doc.data();
+      user.id = doc.id;
+    });
+    return user;
+  }
+
   async createUser(auth) {
     // 作成したコレクションに合わせてオブジェクトを作成する
     const user = {
