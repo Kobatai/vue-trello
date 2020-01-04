7de9bc0d380bc0c1b0c8588928c5f356f85fc6bd
Kobatai <taiga1983346@gmail.com>
2020-01-04 16:52:58 +0900

    キャッシュサービスクラスの作成

---
+++ vue-blog/src/services/CacheService.js
@@ -0,0 +1,25 @@
+class CacheService {
+  getItem(key) {
+    const value = localStorage.getItem(key);
+    // ローカルストレージには文字列でデータを保存するのでJSON形式に復元する
+    // ローカルストレージはユーザーのローカル環境のブラウザにデータを保存できるもので永続的
+    // sessionStorageはウィンドウを閉じるまで
+    // クッキーは指定期限まで
+    return value != null ? JSON.parse(value) : null;
+  }
+
+  setItem(key, value) {
+    if (value != null) {
+      // ローカルストレージには文字列でデータを保存するためJSONから文字列に変換する
+      localStorage.setItem(key, JSON.stringify(value));
+    } else {
+      localStorage.setItem(key, null);
+    }
+  }
+  removeItem(key) {
+    localStorage.removeItem(key);
+  }
+}
+
+const cacheService = new CacheService();
+export { cacheService };
