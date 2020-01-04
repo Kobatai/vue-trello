0bc000c43d57f4aadee069e7d4b83a0c83c41bc0
Kobatai <taiga1983346@gmail.com>
2020-01-04 15:42:38 +0900

    iconコンポーネント修正

--- vue-blog/src/components/Icon.vue
+++ vue-blog/src/components/Icon.vue
@@ -4,19 +4,36 @@
     <!-- @はv-onの省略記法,: はv-bindの省略記法で属性及びコンポーネントプロパティを動的に設定する -->
     <!-- 属性値を<i class="fas fa-{{name}}"></i>とするとエラーになるため -->
     <!-- fasとnameClassをjsに渡してfas fa-userのようなものが返ってきている -->
-    <i :class="['fas', nameClass]"></i>
+    <!-- <i :class="['fas', nameClass]"></i> -->
+
+    <!-- リネームしたclassesを使う -->
+    <i :class="classes"></i>
   </span>
 </template>
 
 <script>
 export default {
   name: "pm-icon",
-  props: { name: String },
+  props: {
+    name: String,
+    // modeプロパティの追加
+    mode: {
+      type: String,
+      validator(val) {
+        return ["fas", "far", "fab"].includes(val);
+      },
+      default: "fas"
+    }
+  },
   // tpl内で読み取れる関数
   // ここではFontawesomeのfaを返す
   computed: {
-    nameClass() {
-      return `fa-${this.name}`;
+    // nameClass() {
+    //   return `fa-${this.name}`;
+    // }
+
+    classes() {
+      return [this.mode, `fa-${this.name}`];
     }
   }
 };
