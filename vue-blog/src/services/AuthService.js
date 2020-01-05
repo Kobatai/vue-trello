0222664298610fb97792207ef1aafcbfbd660926
Kobatai <taiga1983346@gmail.com>
2020-01-05 18:21:17 +0900

    認証情報の更新処理

--- vue-blog/src/services/AuthService.js
+++ vue-blog/src/services/AuthService.js
@@ -20,6 +20,24 @@ class AuthService {
   signOut() {
     return this.auth.signOut();
   }
+
+  // 認証情報更新処理
+  async update(currentPassword, email, password) {
+    const user = this.auth.currentUser;
+    // ユーザーの認証情報を現在のアドレスとPWで取得する
+    const credential = firebase.auth.EmailAuthProvider.credential(
+      user.email,
+      currentPassword
+    );
+    // 再度認証を行う
+    await user.reauthenticateWithCredential(credential);
+    if (email) {
+      await user.updateEmail(email);
+    }
+    if (password) {
+      await user.updatePassword(password);
+    }
+  }
 }
 
 const authService = new AuthService();
